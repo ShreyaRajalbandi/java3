e43ddebee1fea8ee753126af5bba8d470a7a8c3a
ShreyaRajalbandi <rajalabandishreya2005@gmail.com>
2025-04-23 15:23:42 +0530

    saves

--- Demo.java
+++ Demo.java
@@ -5,7 +5,11 @@ class Demo{
         int b=10;
         int sum=a+b;
         int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
         System.out.println("Addition: "+sum);
         System.out.println("Subtraction: "+sub);
+        System.out.println("Multipication: "+mul);
+        System.out.println("Division: "+div);
     }
 }
