d3bd8c95b30e7beccd61c760b667c06bb07aa61f
ShreyaRajalbandi <rajalabandishreya2005@gmail.com>
2025-04-23 15:19:47 +0530

    save

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package java;
+class Demo{
+    public static void main(String[]args){
+        int a=20;
+        int b=10;
+        int sum=a+b;
+        int sub=a-b;
+        System.out.println("Addition: "+sum);
+        System.out.println("Subtraction: "+sub);
+    }
+}
